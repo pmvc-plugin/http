4c5c4b4ea1045ac0a310224749a34eaa48f5a99a
Hill <hill@kimo.com>
2016-02-21 08:00:12 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,29 @@
+[![Latest Stable Version](https://poser.pugx.org/pmvc-plugin/http/v/stable)](https://packagist.org/packages/pmvc-plugin/http) 
+[![Latest Unstable Version](https://poser.pugx.org/pmvc-plugin/http/v/unstable)](https://packagist.org/packages/pmvc-plugin/http) 
+[![Build Status](https://travis-ci.org/pmvc-plugin/http.svg?branch=master)](https://travis-ci.org/pmvc-plugin/http)
+[![License](https://poser.pugx.org/pmvc-plugin/http/license)](https://packagist.org/packages/pmvc-plugin/http)
+[![Total Downloads](https://poser.pugx.org/pmvc-plugin/http/downloads)](https://packagist.org/packages/pmvc-plugin/http) 
+
+http
+===============
+
+## Install with Composer
+### 1. Download composer
+   * mkdir test_folder
+   * curl -sS https://getcomposer.org/installer | php
+
+### 2. Install by composer.json or use command-line directly
+#### 2.1 Install by composer.json
+   * vim composer.json
+```
+{
+    "require": {
+        "pmvc-plugin/http": "dev-master"
+    }
+}
+```
+   * php composer.phar install
+
+#### 2.2 Or use composer command-line
+   * php composer.phar require pmvc-plugin/http
+
---
+++ http.php
@@ -0,0 +1,99 @@
+<?php
+namespace PMVC\PlugIn\http;
+
+${_INIT_CONFIG}[_CLASS] = __NAMESPACE__.'\http';
+
+class http extends \PMVC\PlugIn
+{
+    public function init()
+    {
+        \PMVC\call_plugin(
+            'dispatcher',
+            'attach',
+            array(
+                $this,
+                \PMVC\Event\MAP_REQUEST
+            )
+        );
+    }
+
+    public function onMapRequest()
+    {
+        $controller = \PMVC\getC();
+        $request = $controller->getRequest();
+        $method = $this->getMethod();
+        $request->setMethod($method);
+        if ('GET'===$method) {
+            $inputs =& $_GET;
+        } else {
+            $isJsonInput = ('application/json'===getenv('CONTENT_TYPE'));
+            if ($isJsonInput || 'PUT'===$method) {
+                $input = file_get_contents("php://input");
+                if ($isJsonInput) {
+                    $inputs = (array)fromJson($input);
+                } else {
+                    parse_str($input, $inputs);
+                }
+            } else {
+                $inputs =& $_POST;
+            }
+        }
+        \PMVC\set($request,$inputs);
+    }
+
+    public function getMethod()
+    {
+        $method = getenv('REQUEST_METHOD');
+        $cros_method = getenv('HTTP_ACCESS_CONTROL_REQUEST_METHOD');
+        if ($method === 'OPTIONS' && $cros_method) {
+            $method = $cros_method;
+        }
+        return $method;
+    }
+
+    public function buildCommand($url, $params)
+    {
+        $parsed_url = parse_url($url);
+        if (!empty($params)) {
+            if (!empty($parsed_url['query'])) {
+                parse_str($parsed_url['query'], $parsed_query);
+            } else {
+                $parsed_query = array();
+            }
+            $parsed_url['query'] = http_build_query(
+                array_merge(
+                    $parsed_query,
+                    $params
+                )
+            );
+        }
+        $scheme   = isset($parsed_url['scheme']) ? $parsed_url['scheme'].'://' : ''; 
+        $host     = isset($parsed_url['host']) ? $parsed_url['host'] : ''; 
+        $port     = isset($parsed_url['port']) ? ':' . $parsed_url['port'] : ''; 
+        $user     = isset($parsed_url['user']) ? $parsed_url['user'] : ''; 
+        $pass     = isset($parsed_url['pass']) ? ':' . $parsed_url['pass']  : ''; 
+        $pass     = ($user || $pass) ? "$pass@" : ''; 
+        $path     = isset($parsed_url['path']) ? $parsed_url['path'] : ''; 
+        $query    = isset($parsed_url['query']) ? '?' . $parsed_url['query'] : ''; 
+        $fragment = isset($parsed_url['fragment'])?'#'. $parsed_url['fragment']: ''; 
+        return "$scheme$user$pass$host$port$path$query$fragment"; 
+    }
+
+    /**
+     * Process Header
+     */
+    public function processHeader($headers)
+    {
+        foreach ($headers as $h) {
+            header($h);
+        }
+    }
+
+    /**
+     * execute other php
+     */
+    public function go($path)
+    {
+        $this->processHeader(array("Location: $path"));
+    }
+}
